8c121939be2341bafd8ec9475469fc17ee377edb
SHIROUZU Hiroaki <shirouzu@ipmsg.org>
2020-03-12 21:31:38 +0900

    Add "--add" and "--del" options

--- nsd-upd.py
+++ nsd-upd.py
@@ -6,8 +6,27 @@ import sys
 import re
 import socket
 import time
+import getopt
+
+ADD, MOD, DEL = range(3)
 
 RELOAD_CMD = "/usr/sbin/nsd-control reload"
+
+def get_zonename(d):
+	m = re.search(rb"\$ORIGIN[ \t]+([a-z0-9-.]+)\.", d)
+	if m:
+		zone_name = m.groups()[0]
+	else:
+		m = re.search(rb"([a-z0-9-.]+)\.[ \t]+.*[ \t]SOA[ \t]+", d)
+		if m:
+			zone_name = m.groups()[0]
+
+	if zone_name:
+		# print(zone_name)
+		return zone_name.decode("utf-8")
+
+	raise Exception("zone name can't find")
+
 
 def new_serial(old_s):
 	cur_val = int(old_s)
@@ -19,7 +38,23 @@ def new_serial(old_s):
 		new_s = b'%10d' % (cur_val + 1)
 	return new_s
 
-def main(zone, name="", val=""):
+def serial_update(d):
+	serial_re = re.compile(rb'(IN[ \t]+SOA[^(]+\([ \t\n]*)([0-9]+)([ \t;])', re.I)
+	m = serial_re.search(d)
+	if not m:
+		raise Exception("serial not found")
+	serial = new_serial(m.groups()[1])
+	d = serial_re.sub(rb'\g<1>%s\g<3>' % serial, d, 1)
+	return d, m.groups()[1], serial
+
+def make_zone(zone_name, zone, d):
+	zone_tmp = zone + ".tmp"
+	open(zone_tmp, "wb").write(d)
+	if os.system("nsd-checkzone %s %s" % (zone_name, zone_tmp)):
+		raise Exception("invalid nsd-file(%s)" % zone_tmp)
+	os.rename(zone_tmp, zone)
+
+def modify_rec(zone, name="", val=""):
 	if val == "src":
 		rec = b'A'
 		val = os.environ["SSH_CONNECTION"].split()[0]
@@ -36,15 +71,12 @@ def main(zone, name="", val=""):
 	val = val.encode("utf8")
 	name = name.encode("utf8")
 
+	# ゾーン名取得
 	d = open(zone, "rb").read()
+	zone_name = get_zonename(d)
 
 	# シリアル更新
-	serial_re = re.compile(rb'(IN[ \t]+SOA[^(]+\([ \t\n]*)([0-9]+)([ \t;])', re.I)
-	m = serial_re.search(d)
-	if not m:
-		raise Exception("serial not found")
-	serial = new_serial(m.groups()[1])
-	d = serial_re.sub(rb'\g<1>%s\g<3>' % serial, d, 1)
+	d, old_serial, new_serial = serial_update(d)
 
 	# ターゲット検索
 	if name:
@@ -59,23 +91,77 @@ def main(zone, name="", val=""):
 
 		d = dyn_re.sub(rb'\g<1>%s\g<3>' % val, d, 1)
 	else:
-		old_val = m.groups()[1]
-		val = serial
+		old_val = old_serial
+		val = new_serial
 
-	open(zone, "wb").write(d)
+	make_zone(zone_name, zone, d)
 	if RELOAD_CMD:
 		os.system(RELOAD_CMD)
-	print((b"done(%s) %s -> %s" % (serial, old_val, val)).decode("utf8"))
+	print((b"modify done(%s) %s -> %s" % (new_serial, old_val, val)).decode("utf8"))
+
+
+def add_rec(rec_data, zone):
+	# ゾーン名取得
+	d = open(zone, "rb").read()
+	zone_name = get_zonename(d)
+
+	# シリアル更新
+	d, old_serial, new_serial = serial_update(d)
+
+	rec_data = rec_data.encode("utf8")
+
+	d += rec_data + b"\n"
+
+	make_zone(zone_name, zone, d)
+	if RELOAD_CMD:
+		os.system(RELOAD_CMD)
+	print((b"add done(%s)" % new_serial).decode("utf8"))
+
+def del_rec(rec_data, zone):
+	# ゾーン名取得
+	d = open(zone, "rb").read()
+	zone_name = get_zonename(d)
+	# シリアル更新
+	d, old_serial, new_serial = serial_update(d)
+
+	rec_data = rec_data.encode("utf8")
+
+	sv = d
+	if rec_data[-1:] != b'\n':
+		rec_data += b'\n'
+	d = re.sub(rec_data, b'', d)
+	if d == sv:
+		print("del not found")
+		return
+
+	if len(d) < 100:
+		raise Exception("del illegal")
+
+	make_zone(zone_name, zone, d)
+	if RELOAD_CMD:
+		os.system(RELOAD_CMD)
+	print((b"del done(%s)" % new_serial).decode("utf8"))
+
+
+USAGE = '''\
+usage: nsd-upd.py [hostname (ipaddr|'src'|txt=val)] zone_file
+       nsd-upd.py --add full-record zone_file
+       nsd-upd.py --del regex-pattern zone_file
+'''
 
 if __name__ == "__main__":
 	try:
 		if len(sys.argv) < 2 or not os.access(sys.argv[-1], os.F_OK):
-			raise Exception("usage: nsd-upd.py [hostname (ipaddr|'src'|txt=val)] zone_file")
+			raise Exception(USAGE)
 
-		if len(sys.argv) == 2:
-			main(sys.argv[-1])
+		if   sys.argv[1] == "--add":
+			add_rec(sys.argv[2], sys.argv[3])
+		elif sys.argv[1] == "--del":
+			del_rec(sys.argv[2], sys.argv[3])
+		elif len(sys.argv) == 2:
+			modify_rec(sys.argv[-1])
 		else:
-			main(sys.argv[-1], sys.argv[1], sys.argv[2])
+			modify_rec(sys.argv[-1], sys.argv[1], sys.argv[2])
 		sys.exit(0)
 
 	except Exception as e:
